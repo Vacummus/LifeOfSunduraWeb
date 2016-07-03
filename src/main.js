b8edd6fa928e6cc97e44a6c6ebaa942806c5caf9
Vacummus <vakey.rujevic@gmail.com>
2016-07-03 11:12:15 +0800

    improved the loading of animnated gifs

--- src/main.js
+++ src/main.js
@@ -398,13 +398,69 @@ mainApp.controller('route-controller', ['$scope', '$location', '$http', '$timeou
     });
 }]);
 
-mainApp.controller('overview-controller', ['$scope', function ($scope) {
+mainApp.directive('imageOnload', function() {
+    return {
+        restrict: 'A',
+        link: function($scope, $element, $attrs) {
+            console.log("load image");
+            $element.bind('load', function() {
+                $scope.$evalAsync(function () { $scope.image.isLoaded = true; });
+            });
+            $element.bind('error', function(){
+                console.log('image could not be loaded');
+            });
+        }
+    };
+});
+
+mainApp.controller('overview-controller', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.isPageLoaded = false;
+    var checkOverviewLoaded = function () {
+        if($('.load-test').is(':visible')) {
+            $timeout(function () { $scope.isPageLoaded = true; }, 1000);
+        } else {
+            $timeout(checkOverviewLoaded, 50);
+        }
+    }
+
+    checkOverviewLoaded();
+
     $scope.chapters = [
         { title: "Birth", progress: 1/5 },
         { title: "Growth", progress: 0/5 },
         { title: "Purpose", progress: 0/5 },
         { title: "Death", progress: 0/5 }
-    ]
+    ];
+
+    $scope.animatedGifs = [
+        { coverGif: "/src/content/images/gifs/1-gif-cover.gif", gif: "http://i.imgur.com/sxhAfYn.gif" },
+        { coverGif: "/src/content/images/gifs/2-gif-cover.gif", gif: "http://i.imgur.com/iA9Xcrp.gif" },
+        { coverGif: "/src/content/images/gifs/3-gif-cover.gif", gif: "http://i.imgur.com/T9u9mTe.gif" },
+        { coverGif: "/src/content/images/gifs/4-gif-cover.gif", gif: "http://i.imgur.com/OOLeczl.gif" },
+        { coverGif: "/src/content/images/gifs/5-gif-cover.gif", gif: "http://i.imgur.com/D2h1Gv5.gif" },
+        { coverGif: "/src/content/images/gifs/6-gif-cover.gif", gif: "http://i.imgur.com/MEHTuIl.gif" }
+    ];
+
+    var documentElement = $(document);
+
+    var updateGifVisibility = function () {
+        var scrollTop = documentElement.scrollTop();
+        var scrollBottom = scrollTop + $(window).height();
+
+        $scope.$evalAsync(function () { 
+            for (var i = 0; i < $scope.animatedGifs.length; i++) {
+                var imageElement = $('.gif-images p:nth-child(' + (i + 1) +')');
+                var image = $scope.animatedGifs[i];
+                var imageTop = imageElement.offset().top - 200;
+                var imageBottom = imageTop + imageElement.height() + 200;
+
+                image.visible = imageBottom > scrollTop && scrollBottom > imageTop;
+            }
+        });
+    };
+
+    documentElement.scroll(updateGifVisibility);
+    $(window).resize(updateGifVisibility);
 }]);
 
 var blogData;
