138fa22827adbad454de296280bd020efa407d29
Vacummus <vakey.rujevic@gmail.com>
2016-07-11 10:15:33 +0800

    fixed height issue of logo for phones

--- src/main.js
+++ src/main.js
@@ -19,10 +19,17 @@ var updateHeaderHeight = function () {
 };
 
 $(document).ready(function() {
+    var updateHeight = function (amount) {
+        setTimeout(function() {
+            updateHeaderHeight();
+            if (amount > 0) updateHeight(amount -1);
+        }, 100);
+    }
+
     var initialUpdate = function() {
         setTimeout(function() {
             var imgElement = $('.header .logo-section img');
-            if (imgElement.length > 0 && imgElement[0].complete) updateHeaderHeight();
+            if (imgElement.length > 0 && imgElement[0].complete) updateHeight(20);
             else initialUpdate();
         }, 50);
     };
