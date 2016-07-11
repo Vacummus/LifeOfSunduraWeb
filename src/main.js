ca99a4b00e1437610f60c614b47482f66c61aba8
Vacummus <vakey.rujevic@gmail.com>
2016-07-11 09:56:11 +0800

    fixed issue with logo image not resizing correctly for responsive design

--- src/main.js
+++ src/main.js
@@ -18,7 +18,15 @@ var updateHeaderHeight = function () {
     $('.header').height($('.header .logo-section').outerHeight());
 };
 
-$('.header .logo-section img').on('load', function() {
-    updateHeaderHeight();
+$(document).ready(function() {
+    var initialUpdate = function() {
+        setTimeout(function() {
+            var imgElement = $('.header .logo-section img');
+            if (imgElement.length > 0 && imgElement[0].complete) updateHeaderHeight();
+            else initialUpdate();
+        }, 50);
+    };
+
+    initialUpdate();
     $(window).resize(updateHeaderHeight);
 });
